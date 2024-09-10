bf236712b53422268c888c9808d58f6c0f0f7745
Vinay Saddanapu <chintu2030@gmail.com>
2024-09-10 21:09:46 +0530

    EvenPositive, RangeCheck

---
+++ pyspark_dev/Assignment1/EvenPositive.py
@@ -0,0 +1,7 @@
+
+num = 14
+
+if num > 0 and num%2 == 0:
+    print(True)
+else:
+    print(False)
---
+++ pyspark_dev/Assignment1/RangeCheck.py
@@ -0,0 +1,11 @@
+
+def rangeCheck(number):
+    if number < -10 or number > 10:
+        return True
+    else:
+        return False
+
+
+
+
+print(rangeCheck(-9))
