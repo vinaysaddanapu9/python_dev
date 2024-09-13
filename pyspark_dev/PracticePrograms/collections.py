909cbe2d98a0856524914cd62459d789a1dee733
Vinay Saddanapu <chintu2030@gmail.com>
2024-09-13 09:31:14 +0530

    Collections

---
+++ pyspark_dev/PracticePrograms/collections.py
@@ -0,0 +1,15 @@
+#Create a list
+list = ["apple", "mango", "banana"]
+print("LIST: ",list)
+
+#Create a dictionary
+my_dict = {1: "apple", 2:"mango", 3:"banana"}
+print("DICTIONARY: ",my_dict)
+
+#Create a tuple
+tuple = ("apple", True, 1, 90.0)
+print("TUPLE: ",tuple)
+
+#Create a Set
+set = {1,2,3,4,5}
+print("SET: ", set)
