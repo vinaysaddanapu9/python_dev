6f2978b8fabbaac7946880621aedec09580f66db
Vinay Saddanapu <chintu2030@gmail.com>
2024-09-16 09:59:11 +0530

    Lambda(Anonymous) Functions

---
+++ pyspark_dev/PracticePrograms/lambda_anonymous_func.py
@@ -0,0 +1,13 @@
+#Square of a number
+square = lambda a: a*a
+
+print("Square:", square(8))
+
+#Cube of a number
+cube = lambda c : c*c*c
+print("Cube: ", cube(3))
+
+#Sum of numbers
+sum = lambda a,b : a+b
+print("Sum:", sum(7,7))
+
