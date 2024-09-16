d5bc8b51673c73dae616714bace2862152ffc23b
Vinay Saddanapu <chintu2030@gmail.com>
2024-09-16 21:11:53 +0530

    Word Count

---
+++ pyspark_dev/spark_core/word_count.py
@@ -0,0 +1,15 @@
+#Word Count in PySpark
+from pyspark import SparkContext
+import os
+
+os.environ[ "PYSPARK_PYTHON"] = "E:/Python/Python/Python37/python.exe"
+
+sc = SparkContext("local[4]", "SparkRDD")
+rdd = sc.textFile("E:/Files/data5.txt")
+rdd2 = rdd.flatMap(lambda x : x.split(" "))
+rdd3 = rdd2.map(lambda x: (x,1))
+rdd4 = rdd3.reduceByKey(lambda x,y : x+y)
+rdd5 = rdd4.sortBy(lambda x: x[0], True)
+
+for i in rdd5.collect():
+    print(i)
